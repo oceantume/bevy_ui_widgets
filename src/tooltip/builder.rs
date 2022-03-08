1fbf65371c0f73c700547834f7b1320cc3fb2e00
Gabriel Bourgeois <gabriel.bourgeoisv4si@gmail.com>
2022-03-07 20:29:54 -0500

    refactor(tooltip): simplify builder with new WidgetBuilderEntity

--- src/tooltip/builder.rs
+++ src/tooltip/builder.rs
@@ -9,16 +9,15 @@ use crate::utils::*;
 use super::*;
 
 pub struct TooltipWidgetBuilder<'a, 'w, 's> {
-    root_bundle: Option<TooltipBundle>,
+    root: WidgetBuilderEntity<'a, 'w, 's, Option<TooltipBundle>>,
     content_entity: Option<Entity>,
-    root_commands_runners: EntityCommandsRunnersVec<'a, 'w, 's>,
 }
 
 impl<'a, 'w, 's> TooltipWidgetBuilder<'a, 'w, 's> {
     /// Creates a new tooltip builder
     pub fn new() -> TooltipWidgetBuilder<'a, 'w, 's> {
         Self {
-            root_bundle: Some(TooltipBundle {
+            root: WidgetBuilderEntity::new(Some(TooltipBundle {
                 style: Style {
                     position_type: PositionType::Absolute,
                     border: Rect::all(Val::Px(2.0)),
@@ -26,9 +25,8 @@ impl<'a, 'w, 's> TooltipWidgetBuilder<'a, 'w, 's> {
                     ..default()
                 },
                 ..default()
-            }),
+            })),
             content_entity: None,
-            root_commands_runners: default(),
         }
     }
 
@@ -37,7 +35,7 @@ impl<'a, 'w, 's> TooltipWidgetBuilder<'a, 'w, 's> {
         &mut self,
         run_commands: impl for<'b> Fn(&mut EntityCommands<'w, 's, 'b>) + 'a,
     ) -> &mut Self {
-        self.root_commands_runners.push(Box::new(run_commands));
+        self.root.commands_runners.push(Box::new(run_commands));
         self
     }
 
@@ -47,7 +45,7 @@ impl<'a, 'w, 's> TooltipWidgetBuilder<'a, 'w, 's> {
         &mut self,
         extend: impl FnOnce(TooltipBundle) -> TooltipBundle,
     ) -> &mut Self {
-        self.root_bundle = Some(extend(std::mem::take(&mut self.root_bundle).unwrap()));
+        self.root.bundle = Some(extend(std::mem::take(&mut self.root.bundle).unwrap()));
         self
     }
 
@@ -62,10 +60,11 @@ impl<'a, 'w, 's> TooltipWidgetBuilder<'a, 'w, 's> {
     /// Using the builder again after calling this will panic.
     pub fn spawn(&mut self, commands: &'a mut Commands<'w, 's>) -> Entity {
         let root_entity = commands
-            .spawn_bundle(std::mem::take(&mut self.root_bundle).unwrap())
+            .spawn_bundle(std::mem::take(&mut self.root.bundle).unwrap())
             .id();
 
-        self.root_commands_runners
+        self.root
+            .commands_runners
             .iter()
             .for_each(|run| run(&mut commands.entity(root_entity)));
 
